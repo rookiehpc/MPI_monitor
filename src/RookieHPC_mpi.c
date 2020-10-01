bc7f7fe4048faf1b0f081d4b9a7db7d243ae86c3
RookieHPC <github@rookiehpc.com>
2020-10-01 22:44:35 +0100

    MPI_Scatterv added.

--- src/RookieHPC_mpi.c
+++ src/RookieHPC_mpi.c
@@ -125,6 +125,8 @@ enum RookieHPC_MPI_message_type_t { /// The message is sent about MPI_Allgather
                                     ROOKIEHPC_MESSAGE_SCAN,
                                     /// The message is sent about MPI_Scatter
                                     ROOKIEHPC_MESSAGE_SCATTER,
+                                    /// The message is sent about MPI_Scatterv
+                                    ROOKIEHPC_MESSAGE_SCATTERV,
                                     /// The message is sent about MPI_Send
                                     ROOKIEHPC_MESSAGE_SEND,
                                     /// The message is sent about MPI_Sendrecv
@@ -184,6 +186,7 @@ const char* RookieHPC_MPI_routine_name_t[] = { "MPI_Allgather",
                                                "MPI_Rsend",
                                                "MPI_Scan",
                                                "MPI_Scatter",
+                                               "MPI_Scatterv",
                                                "MPI_Send",
                                                "MPI_Sendrecv",
                                                "MPI_Sendrecv_replace",
@@ -811,6 +814,14 @@ int RookieHPC_MPI_Scatter(const void* buffer_send, int count_send, MPI_Datatype 
     RookieHPC_monitoring_message(ROOKIEHPC_TEMPORALITY_BEFORE, ROOKIEHPC_MESSAGE_SCATTER, file, line, args);
     int result = MPI_Scatter(buffer_send, count_send, datatype_send, buffer_recv, count_recv, datatype_recv, root, communicator);
     RookieHPC_monitoring_message(ROOKIEHPC_TEMPORALITY_AFTER, ROOKIEHPC_MESSAGE_SCATTER, file, line, args);
+    return result;
+}
+
+int RookieHPC_MPI_Scatterv(const void* buffer_send, const int counts_send[], const int displacements[], MPI_Datatype datatype_send, void* buffer_recv, int count_recv, MPI_Datatype datatype_recv, int root, MPI_Comm communicator, char* file, int line, const char* args)
+{
+    RookieHPC_monitoring_message(ROOKIEHPC_TEMPORALITY_BEFORE, ROOKIEHPC_MESSAGE_SCATTERV, file, line, args);
+    int result = MPI_Scatterv(buffer_send, counts_send, displacements, datatype_send, buffer_recv, count_recv, datatype_recv, root, communicator);
+    RookieHPC_monitoring_message(ROOKIEHPC_TEMPORALITY_AFTER, ROOKIEHPC_MESSAGE_SCATTERV, file, line, args);
     return result;
 }
 
